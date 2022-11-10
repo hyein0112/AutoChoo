81384214ef052b9126d0b690d9c44a4dbf11cc12
hyein0112 <bhi12134@gmail.com>
2022-11-10 19:52:06 +0900

    Title margin값 변경

--- src/components/Title/style.js
+++ src/components/Title/style.js
@@ -3,5 +3,5 @@ import styled from '@emotion/styled';
 export const Description = styled.p`
   font-family: AppleSDGothicNeo-Medium, -apple-system, sans-serif;
   font-size: 1.4rem;
-  margin-top: 30px;
+  margin-top: 25px;
 `;
